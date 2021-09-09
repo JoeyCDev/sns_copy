c611343fdecaf9e7913db57401f69570fecc405c
joseph58080 <joseph58080@gmail.com>
2021-09-09 13:49:47 +0900

    [최우영] - SNS copy project 환경설정

---
+++ src/test/java/com/majon/sns_copy/SnsCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majon.sns_copy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
